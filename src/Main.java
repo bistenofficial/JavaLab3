c6d5702d5a805eeb522f9d7c7483b8cf02793534
bistenofficial <maks.kirov.2002@mail.ru>
2021-09-09 11:50:08 +0300

    Остались комментарии

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,8 @@ public class Main
     public static void main(String[] args) {
         ArrayList<String> Key = new ArrayList<String>();
         ArrayList<String> Count = new ArrayList<String>();
-        try (BufferedReader br = new BufferedReader(new FileReader("src/Input"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("src/Input")))
+        {
             String s;
             while ((s = br.readLine()) != null) {
                 String[] Values = s.split(" ");
@@ -23,19 +24,27 @@ public class Main
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-        for (int i = 0; i < Key.size(); i++) {
-            if (Integer.parseInt(Count.get(i)) == 1) {
-                for (int j = i + 1; j < Key.size(); j++) {
-                    if (Key.get(i).equals(Key.get(j))) {
+        for (int i = 0; i < Key.size(); i++)
+        {
+            if (Integer.parseInt(Count.get(i)) == 1)
+            {
+                for (int j = i + 1; j < Key.size(); j++)
+                {
+                    if (Key.get(i).equals(Key.get(j)))
+                    {
                         if (Integer.parseInt(Count.get(j)) == 1) System.out.println("YES1");
                     }
                 }
             }
         }
-        for (int i = 0; i < Key.size(); i++) {
-            if (Integer.parseInt(Count.get(i)) == -1) {
-                for (int j = i + 1; j < Key.size(); j++) {
-                    if (Key.get(i).equals(Key.get(j))) {
+        for (int i = 0; i < Key.size(); i++)
+        {
+            if (Integer.parseInt(Count.get(i)) == -1)
+            {
+                for (int j = i + 1; j < Key.size(); j++)
+                {
+                    if (Key.get(i).equals(Key.get(j)))
+                    {
                         if (Integer.parseInt(Count.get(j)) == -1) System.out.println("YES");
                     }
                 }
